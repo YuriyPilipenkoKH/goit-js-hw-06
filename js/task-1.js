9fbd94fa434e442c34be642640a692d4460561c8
Yuriy Pilipenko <yurik2061@gmail.com>
2023-02-01 17:47:40 +0200

    files added

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+const categoryItems = document.querySelectorAll('#categories > li');
+console.log('Number of categories:',categoryItems.length);
+
+const itemTiteles = document.querySelectorAll('.item > h2');
+const categoryElements = document.querySelectorAll('.item > ul ');
+const items = document.querySelectorAll('.item ');
+// console.log(items);
+// childElementCount
+
+for (let i = 0; i < items.length; i+=1) {
+    const element = items[i];
+    console.log('Category:',itemTiteles[i].textContent );
+    console.log('Elements:',categoryElements[i].childElementCount );
+    
+}
---
+++ js/task-2.js
@@ -0,0 +1,25 @@
+const ingredients = [
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatos",
+    "Herbs",
+    "Condiments",
+  ];
+  
+  const ingredientsList = document.getElementById("ingredients");
+  const elements = []
+
+ingredients.forEach(ingredient => {
+    const item = document.createElement('li');
+    item.textContent = ingredient;
+    item.class = 'item';
+    // console.log(item);
+    elements.push(item)
+
+  });
+
+    // console.log(elements)
+    ingredientsList.append(...elements);
+
+    console.log(ingredientsList);
