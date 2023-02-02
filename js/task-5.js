63e0f6794139434714a94dd7618d2dcc9d4f6994
Yuriy Pilipenko <yurik2061@gmail.com>
2023-02-02 15:38:34 +0200

    6-input

--- js/task-5.js
+++ js/task-5.js
@@ -3,14 +3,10 @@ const textInput = document.getElementById("name-input");
 const output = document.getElementById("name-output");
 
 textInput.addEventListener("input", (event) => {
-    // if (event.currentTarget.value !== "") {
-    //     output.textContent = event.currentTarget.value;
-    // } else {
-    //     output.textContent = "Anonymous";
-    // }
+   
 
     event.currentTarget.value !== "" 
         ?  output.textContent = event.currentTarget.value 
         :  output.textContent = "Anonymous"
     }
-  );
+  )
---
+++ js/task-6.js
@@ -0,0 +1,13 @@
+const inputToValidate = document.querySelector('#validation-input');
+// console.log(inputToValidate);
+
+inputToValidate.addEventListener("blur", (event) => {
+    if (event.currentTarget.value.length !== Number(inputToValidate.dataset.length)) {
+        inputToValidate.classList.remove('valid');
+        inputToValidate.classList.add('invalid');
+        } else {
+        inputToValidate.classList.remove('invalid');
+        inputToValidate.classList.add('valid');
+    }
+    }
+  );
