b76146d1ef61575560c3989ed924bc4ffeda4cd4
Yuriy Pilipenko <yurik2061@gmail.com>
2023-02-03 14:14:20 +0200

    console

--- js/task-08.js
+++ js/task-08.js
@@ -5,15 +5,17 @@ form.addEventListener('submit', onFormSubmit)
 function onFormSubmit(event) {
     event.preventDefault();
 
-
-    if (event.currentTarget.elements[0].value === "" || event.currentTarget.elements[1].value === "") {
-        alert("Field is empty");
-    };
-
     const mail = event.currentTarget.elements.email.value;
     const password = event.currentTarget.elements.password.value;
 
-    console.log({mail, password});
+    if (event.currentTarget.elements[0].value === "" || event.currentTarget.elements[1].value === "") {
+        alert("Field is empty");
+    } else {
+        console.log({mail, password});
+    }
+
 
     form.reset()
 }
+
+
